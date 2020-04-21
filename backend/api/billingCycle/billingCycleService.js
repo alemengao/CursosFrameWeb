02a6e9603560a200d2255e922c3ab8e95d0602b8
alemengao <alessandro_cs@yahoo.com.br>
2020-04-21 13:47:44 -0300

    erro gulp

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,16 +1,35 @@
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
-BillingCycle.methods = (['get','post','put','delete'])
-//BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.methods(['get','post','put','delete'])
+BillingCycle.updateOptions({new: true, runValidators: true})
 
-//BillingCycle.route('count', function (req, res, next) {
-//    BillingCycle.count(function (error, value) {
-//       if (error) {
-//          res.status(500).json({errors: [error]})
-//       }else {
-//          res.json({value})  
-//       }  
-//     })
-//})
+function sendErrosOrNext(req, res, next){
+  const bundle = res.locals.bundle;
+
+  if (bundle.errors){
+    var errors=parseErrors(blundle.errors)
+    res.status(500).json({errors})
+  } else {
+    next()
+  }
+}
+
+function parseErrors(nodeRestfulErrors) {
+     const errors = []
+      _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+       return errors
+}
+
+
+BillingCycle.route('count', function (req, res, next) {
+    BillingCycle.count(function (error, value) {
+       if (error) {
+          res.status(500).json({errors: [error]})
+       }else {
+          res.json({value})  
+       }  
+     })
+})
 
 module.exports = BillingCycle
